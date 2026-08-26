4ec774a882922101448974da2182a6e99486f0d2
Ana Moreno <damimoreno38@gmail.com>
2026-08-26 12:25:42 -0600

    Actualizacion de base de datos smartbase

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'          => '',
         'hostname'     => 'localhost',
         'username'     => 'root',                         
-        'password'     => '',                             
+        'password'     => 'admin123',                             
         'database'     => 'smartbase',  
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
